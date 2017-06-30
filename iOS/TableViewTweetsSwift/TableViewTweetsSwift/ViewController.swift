403353de79f46f62c843180cfe956cc0d8b2c1e7
Andy Piper <apiper@twitter.com>
2017-06-30 10:21:57 +0100

    Convert iOS sample to TK3 and Swift 3

--- iOS/TableViewTweetsSwift/TableViewTweetsSwift/ViewController.swift
+++ iOS/TableViewTweetsSwift/TableViewTweetsSwift/ViewController.swift
@@ -25,13 +25,15 @@ class ViewController: UITableViewController , TWTRTweetViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.tableView.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
+        
         // Create a single prototype cell for height calculations.
-        self.prototypeCell = TWTRTweetTableViewCell(style: .Default, reuseIdentifier: tweetTableCellReuseIdentifier)
+        self.prototypeCell = TWTRTweetTableViewCell(style: .default, reuseIdentifier: tweetTableCellReuseIdentifier)
 
         // Register the identifier for TWTRTweetTableViewCell.
-        self.tableView.registerClass(TWTRTweetTableViewCell.self, forCellReuseIdentifier: tweetTableCellReuseIdentifier)
-        // Setup table data
+        self.tableView.register(TWTRTweetTableViewCell.self, forCellReuseIdentifier: tweetTableCellReuseIdentifier)
 
+        // Setup table data
         loadTweets()
 
     }
@@ -41,11 +43,11 @@ class ViewController: UITableViewController , TWTRTweetViewDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-    override func viewDidAppear(animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
         // Make sure the navigation bar is not translucent when scrolling the table view.
-        self.navigationController?.navigationBar.translucent = false
+        self.navigationController?.navigationBar.isTranslucent = false
     }
 
 
@@ -57,27 +59,23 @@ class ViewController: UITableViewController , TWTRTweetViewDelegate {
         self.isLoadingTweets = true
 
         // set tweetIds to find
-        var tweetIDs = ["266031293945503744", "440322224407314432"];
+        let tweetIDs = ["20","266031293945503744", "440322224407314432"];
 
-        // load tweets with guest login
-        Twitter.sharedInstance().logInGuestWithCompletion { (session: TWTRGuestSession!, error: NSError!) in
 
-            // Find the tweets with the tweetIDs
-            Twitter.sharedInstance().APIClient.loadTweetsWithIDs(tweetIDs) {
-                (twttrs, error) -> Void in
-
-                // If there are tweets do something magical
-                if ((twttrs) != nil) {
-
-                    // Loop through tweets and do something
-                    for i in twttrs {
-                        // Append the Tweet to the Tweets to display in the table view.
-                        self.tweets.append(i as TWTRTweet)
-                    }
-                } else {
-                    println(error)
+        // Find the tweets with the tweetIDs
+        let client = TWTRAPIClient()
+        client.loadTweets(withIDs: tweetIDs) { (twttrs, error) -> Void in
+            
+            // If there are tweets do something magical
+            if ((twttrs) != nil) {
+                
+                // Loop through tweets and do something
+                for i in twttrs! {
+                    // Append the Tweet to the Tweets to display in the table view.
+                    self.tweets.append(i as TWTRTweet)
                 }
-
+            } else {
+                print(error as Any)
             }
         }
 
@@ -89,24 +87,24 @@ class ViewController: UITableViewController , TWTRTweetViewDelegate {
     }
 
     // MARK: TWTRTweetViewDelegate
-    func tweetView(tweetView: TWTRTweetView!, didSelectTweet tweet: TWTRTweet!) {
+    func tweetView(_ tweetView: TWTRTweetView!, didSelect tweet: TWTRTweet!) {
         // Display a Web View when selecting the Tweet.
         let webViewController = UIViewController()
         let webView = UIWebView(frame: webViewController.view.bounds)
-        webView.loadRequest(NSURLRequest(URL: tweet.permalink))
+        webView.loadRequest(URLRequest(url: tweet.permalink))
         webViewController.view = webView
         self.navigationController?.pushViewController(webViewController, animated: true)
     }
 
     // MARK: UITableViewDataSource
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Return the number of Tweets.
         return tweets.count
     }
 
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Retrieve the Tweet cell.
-        let cell = tableView.dequeueReusableCellWithIdentifier(tweetTableCellReuseIdentifier, forIndexPath: indexPath) as TWTRTweetTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: tweetTableCellReuseIdentifier, for: indexPath) as! TWTRTweetTableViewCell
 
         // Assign the delegate to control events on Tweets.
         cell.tweetView.delegate = self
@@ -115,21 +113,18 @@ class ViewController: UITableViewController , TWTRTweetViewDelegate {
         let tweet = tweets[indexPath.row]
 
         // Configure the cell with the Tweet.
-        cell.configureWithTweet(tweet)
+        cell.configure(with: tweet)
 
         // Return the Tweet cell.
         return cell
     }
 
     // MARK: UITableViewDelegate
-    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         let tweet = self.tweets[indexPath.row]
-        self.prototypeCell?.configureWithTweet(tweet)
-        if let height = self.prototypeCell?.calculatedHeightForWidth(self.view.bounds.width) {
-            return height
-        } else {
-            return self.tableView.estimatedRowHeight
-        }
+        self.prototypeCell?.configure(with: tweet)
+        
+        return TWTRTweetTableViewCell.height(for: tweet, style: TWTRTweetViewStyle.compact, width: self.view.bounds.width, showingActions: false)
     }
 }
 
